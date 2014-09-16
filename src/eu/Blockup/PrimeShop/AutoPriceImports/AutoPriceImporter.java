70fbbfd17f894f86326068264da426901c93abbe
JonasHess <EDMAN@blockup.eu>
2014-09-16 11:32:20 +0200

    Added template for AutoPirce imports TAKE 2

--- src/eu/Blockup/PrimeShop/AutoPriceImports/AutoPriceImporter.java
+++ src/eu/Blockup/PrimeShop/AutoPriceImports/AutoPriceImporter.java
@@ -8,16 +8,17 @@ public class AutoPriceImporter {
 
     
     private void import_Item (ItemStack itemStack, double currentPrice) {
-        
+        // I'll do this part.
     }
     
     private void import_Shop (String shopName, List<ItemStack> list_with_items, ItemStack displayItem) {
-        
+     // I'll do this part.
     }
     
     
     
     public void start_Import () {
+        // You do this
         
     }
     
